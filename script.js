1ffbcfd87abcc33eae17b4f3d5565bd67ce6fc7b
barkey2121 <51538026+barkey2121@users.noreply.github.com>
2021-11-14 19:03:35 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+
+    //validates amount input
     if(principal <= 0 || principal == "")
     {
-        alert("Re-enter a valid number <= 0");
-        //document.getElementById("principal").focus();
+        alert("Enter a positive number")
+        document.getElementById("principal").focus();
         return;
     }
 
+    //declares variables
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
-    var dateNow = new Date();
-    var yearNow = parseInt(dateNow.getFullYear())+parseInt(years);
 
+    //sets dates/years info
+    var date = new Date();
+    var year = parseInt(date.getFullYear()) + parseInt(years);
+
+    //displays output
     var finalOutput = document.getElementById("result");
-    finalOutput.innerHTML="If you deposit "+"<span class='highlight'>"+principal+"</span>"+","
-        "at an interest rate of "+"<span class='highlight'>"+rate+"</span>%."+""
-        "You will receive an amount of "+"<span class='highlight'>"+interest+"</span>"+","
-        "in the year "+"<span class='highlight'>"+yearNow+"</span>";
+    finalOutput.innerHTML="If you deposit "+"<span class='highlight'>"+principal+"</span>."+", <br> at an interest rate of "+"<span class='highlight'>"+rate+"</span>%."+"<br> You will receive an amount of "+"<span class='highlight'>"+interest+"</span>"+", <br> in the year "+"<span class='highlight'>"+year+"</span>";
 }
+    //function to update rate
     function updateRate() 
-    {
+{
         var rateval = document.getElementById("rate").value;
         document.getElementById("rate_val").innerText=rateval;
-    }
-
+}
