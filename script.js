c8d157b15fc8a047854340c288192ae33c1e755d
barkey2121 <51538026+barkey2121@users.noreply.github.com>
2021-11-15 14:59:00 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function compute()
     var year = parseInt(date.getFullYear())+parseInt(years);
     //displays output
     var finalOutput = document.getElementById("result");
-    finalOutput.innerHTML="If you deposit "+"<span class='highlight'>"+principal+"</span>."+", <br> at an interest rate of "+"<span class='highlight'>"+rate+"</span>%."+"<br> You will receive an amount of "+"<span class='highlight'>"+interest+"</span>"+", <br> in the year "+"<span class='highlight'>"+year+"</span>";
+    finalOutput.innerHTML="If you deposit "+"<span class='highlight'>"+principal+"</span>"+", <br> at an interest rate of "+"<span class='highlight'>"+rate+"</span>%."+"<br> You will receive an amount of "+"<span class='highlight'>"+interest+"</span>"+", <br> in the year "+"<span class='highlight'>"+year+"</span>.";
 }
     //function to update rate
     function updateRate() 
